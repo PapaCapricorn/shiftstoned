112307748a7651c6e2443fc15be047c33aa498ae
PapaCapricorn <39966530+PapaCapricorn@users.noreply.github.com>
2020-04-26 14:00:57 -0400

    Delete .github/utils

--- .github/utils/shortcodes/icon.js
+++
@@ -1,4 +0,0 @@
-// make faction icon
-module.exports = (faction) => {
-  return `<img alt="${faction}" src="https://www.shiftstoned.com/epc/icon-${faction.toLowerCase()}.png" style="width: 19px; height: 19px;" />`;
-};
