c6b85c6baa157126be7245b64164adbd13612187
PapaCapricorn <39966530+PapaCapricorn@users.noreply.github.com>
2020-05-13 09:45:09 -0400

    Faction icon shortcode src changed to /epc/images

--- src/utils/shortcodes/icon.js
+++ src/utils/shortcodes/icon.js
@@ -1,4 +1,4 @@
 // make faction icon
 module.exports = (faction) => {
-  return `<img alt="${faction}" src="https://www.shiftstoned.com/epc/icon-${faction.toLowerCase()}.png" style="width: 19px; height: 19px;" />`;
+  return `<img alt="${faction}" src="/epc/images/icon-${faction.toLowerCase()}.png" style="width: 19px; height: 19px;" />`;
 };
