9bb2f1066b5a110eb868e0e8dffb24be573f063d
SRFS <sforrestersims@gmail.com>
2020-06-24 22:29:04 -0400

    toggles etc

---
+++ src/site/_includes/js/page/formatUpdates.js
@@ -0,0 +1,8 @@
+// Header JS
+  // toggle menu
+  // initial "header.dataset.state == 'closed'"
+  var toggleSections = document.querySelectorAll(`[data-show-states]`);
+  
+  toggleSections.forEach(setSections = () => {
+    
+  });
