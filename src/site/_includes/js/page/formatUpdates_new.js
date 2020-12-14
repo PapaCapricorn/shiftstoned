1f1d4eae2dd8c6f7bc1a64b24b117485526e70de
SRFS <sforrestersims@gmail.com>
2020-12-13 21:24:32 -0500

    preperation for the EoG release's Updates article

---
+++ src/site/_includes/js/page/formatUpdates_new.js
@@ -0,0 +1,97 @@
+// Card toggle filters
+const toggleSections = document.querySelectorAll(`[data-show-states]`);
+const factionFilters = document.querySelector(`#factionFilters`).querySelectorAll(`[id^='filter-']`);
+const stateFilters = document.querySelector(`#stateFilters`).querySelectorAll(`[id^='filter-']`);
+const rarityFilters = document.querySelector(`#rarityFilters`).querySelectorAll(`[id^='filter-']`);
+const weightingFilters = document.querySelector(`#weightingFilters`).querySelectorAll(`[id^='filter-']`);
+const weightingChangeFilters = document.querySelector(`#weightingChangeFilters`).querySelectorAll(`[id^='filter-']`);
+var formatFilters = document.querySelector(`#formatFilter`);
+
+// format Filters may not be used in page output. if missing, we need to skip
+if (formatFilters !== null) {
+  formatFilters = formatFilters.querySelectorAll(`[id^='filter-']`);
+  formatFilters.forEach((toggle) => {
+    toggle.addEventListener('change', () => {
+      let update = toggle.dataset.filterFormat;
+    
+      toggleSections.forEach((section) => {
+        section.dataset.showFormat = update;
+      });
+    });
+  });
+}
+
+let factionsShown = [];
+let statesShown = [];
+let raritiesShown = [];
+let weightingsShown = [];
+let weightingChangesShown = [];
+
+function show(arr, value) {
+  if (!arr.includes(value)) { arr = arr.push(value)}
+};
+
+function hide(arr, value) {
+  const index = arr.indexOf(value);
+  if (index > -1) {
+    arr = arr.splice(index, 1);
+  }
+};
+
+factionFilters.forEach((toggle) => {
+  let faction = toggle.dataset.filterFaction;
+  show( factionsShown, faction);
+  toggle.addEventListener('change', () => {
+    updateFilters(toggle, factionsShown, 'filterFaction', 'showFactions');
+  });
+});
+
+stateFilters.forEach((toggle) => {
+  let state = toggle.dataset.filterState;
+  show( statesShown, state);
+  toggle.addEventListener('change', () => {
+    updateFilters(toggle, statesShown, 'filterState', 'showStates');
+  });
+});
+
+rarityFilters.forEach((toggle) => {
+  let state = toggle.dataset.filterRarity;
+  show( raritiesShown, state);
+  toggle.addEventListener('change', () => {
+    updateFilters(toggle, raritiesShown, 'filterRarity', 'showRarities');
+  });
+});
+
+weightingFilters.forEach((toggle) => {
+  let state = toggle.dataset.filterWeighting;
+  show( weightingsShown, state);
+  toggle.addEventListener('change', () => {
+    updateFilters(toggle, weightingsShown, 'filterWeighting', 'showWeightings');
+  });
+});
+
+weightingChangeFilters.forEach((toggle) => {
+  let state = toggle.dataset.filterDraftKeptChange;
+  show( weightingChangesShown, state);
+  toggle.addEventListener('change', () => {
+    updateFilters(toggle, weightingChangesShown, 'filterDraftKeptChange', 'showDraftKeptChanges');
+  });
+});
+
+function updateFilters(toggle, array, toggleValue, sectionValue) {
+  let update = toggle.dataset[toggleValue];
+  toggle.checked ? show( array, update) : hide( array, update);
+  toggleSections.forEach((section) => {
+    section.dataset[sectionValue] = array.join(' ');
+  });
+};
+
+// const mainSection = document.querySelector(`#updateList`);
+// var weightChangesToggle = document.querySelector(`#weightChangeFilter`);
+// if (weightChangesToggle !== null) {
+//   weightChangesToggle.addEventListener('change', () => {
+//     weightChangesToggle.checked
+//       ? mainSection.dataset.showKeptUnchanged = true
+//       : mainSection.dataset.showKeptUnchanged = false;
+//   });
+// };
---
+++ src/site/articles/format-updates/2020-12-14-formatUpdatesEmpireofGlass.md
@@ -0,0 +1,30 @@
+---
+title: "Format Updates - Empire of Glass"
+description: "Explore the changes to **Expedition** and **Draft Packs** with the release of Eternal's newest set in this **interactive gallery**."
+articleImage: "/images/articles/formatUpdatesArgentDepths.jpg"
+articleTitle: "Empire of Glass Format Updates"
+articleSubtitle: "Changes to Expedition and Draft Packs with the release of Eternal's newest set"
+layout: "layouts/formatUpdates_new.njk"
+---
+**{% date page.date %}** - With each large expansion, Dire Wolf Digital redesigns the *Eternal* Draft Packs and Expedition to complement the new set. Draft and Expedition are [now entirely new formats][EoG updates]{target="_blank} with [*Empire of Glass'*][EoG]{target="_blank} release, so let's explore the changes.
+
+ [EoG updates]: https://www.direwolfdigital.com/news/argent-depths-format-updates/
+ [EoG]: https://direwolfdigital.com/news/new-set-empire-of-glass/
+
+The official list of current Draft Pack contents and Expedition-legal cards can always be found [on DWD's site][Card List]{target="_blank}. Our trusty Grenadin have processed that info, compared it to the previous formats, and compiled the changes into this handy interactive gallery for you.
+
+ [Card List]: https://www.direwolfdigital.com/news/draft-packs-card-list/
+
+Use the toggles below to swap between format views, and to filter cards based on their factions, draft weightings, or changes. A few notes:
+
+* The Draft Packs toggle only shows the Draft Pack changes (not *Empire of Glass*) between this new format and the *Argent Depths* format's [November 2020 update][] (we are ignoring the special *Empire of Glass* Draft Preview).
+* The Draft Packs toggle displays the cards' weightings *(weighting affects the rate a card appears in draft&mdash;for example, cards with a weight of 5x are five times more likely to appear in Draft Packs than cards with a weight of 1x at the same rarity)*.
+    * With the Draft Pack toggle on, additional toggles are shown for each weighting, and for weighting changes to cards kept in the draft pack.
+* The Expedition changes **do not** include the following sets, which remain in the format in their entirety:
+    * *Argent Depths*, including its promos (from Coslo, Friendly Scavenger onward)
+    * *Awakening*
+    * *Bastion Rising*
+    * *Empire of Glass*
+* The Expedition changes **do** include pre-*Argent Depths* promos, *Echoes of Eternity*, Draft Pack updates, and cards from older campaign and bundle sets.
+
+ [November 2020 update]: /articles/FormatUpdatesNovember2020/
